a40178adc8a9c46a60fdd977c981724d23190f52
Daniel Turner <dan@multiverse.io>
2022-07-21 17:10:34 +0100

    add files

---
+++ src/parsers/strategies_parser.rs
@@ -0,0 +1,231 @@
+use crate::parsers::strategy_structs::*;
+use std::collections::HashMap;
+
+pub fn parse(
+    strategies: Vec<StrategyInFile>,
+    transformer_overrides: TransformerOverrides,
+) -> HashMap<String, HashMap<String, ColumnInfo>> {
+    let mut transformed_strategies: HashMap<String, HashMap<String, ColumnInfo>> = HashMap::new();
+    //TODO If all columns are none, lets not do any transforming?
+    for strategy in strategies {
+        let columns = strategy
+            .columns
+            .into_iter()
+            .map(|column| {
+                (
+                    column.name.clone(),
+                    ColumnInfo {
+                        data_category: column.data_category.clone(),
+                        transformer: transformer(column, &transformer_overrides),
+                    },
+                )
+            })
+            .collect();
+
+        transformed_strategies.insert(strategy.table_name, columns);
+    }
+
+    transformed_strategies
+}
+
+fn column_transformer_is_overriden(
+    data_category: DataCategory,
+    overrides: &TransformerOverrides,
+) -> bool {
+    (data_category == DataCategory::PotentialPii && overrides.allow_potential_pii)
+        || (data_category == DataCategory::CommerciallySensitive
+            && overrides.allow_commercially_sensitive)
+}
+fn transformer(column: ColumnInFile, overrides: &TransformerOverrides) -> Transformer {
+    if column_transformer_is_overriden(column.data_category, overrides) {
+        Transformer {
+            name: TransformerType::Identity,
+            args: None,
+        }
+    } else {
+        column.transformer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TABLE_NAME: &str = "gert_lush_table";
+    const PII_COLUMN_NAME: &str = "pii_column";
+    const COMMERCIALLY_SENSITIVE_COLUMN_NAME: &str = "commercially_sensitive_column";
+
+    #[test]
+    fn can_parse_file_contents_into_hashmaps() {
+        let column_name = "column1";
+
+        let strategies = vec![StrategyInFile {
+            table_name: TABLE_NAME.to_string(),
+            description: "description".to_string(),
+            columns: vec![column_in_file(
+                DataCategory::Pii,
+                column_name,
+                TransformerType::Scramble,
+            )],
+        }];
+
+        let expected = HashMap::from([(
+            TABLE_NAME.to_string(),
+            HashMap::from([(
+                column_name.to_string(),
+                ColumnInfo {
+                    data_category: DataCategory::Pii,
+                    transformer: Transformer {
+                        name: TransformerType::Scramble,
+                        args: None,
+                    },
+                },
+            )]),
+        )]);
+        let parsed = parse(strategies, TransformerOverrides::none());
+        assert_eq!(expected, parsed);
+    }
+
+    #[test]
+    fn ignores_transformers_for_potential_pii_if_flag_provided() {
+        let strategies = vec![StrategyInFile {
+            table_name: TABLE_NAME.to_string(),
+            description: "description".to_string(),
+            columns: vec![
+                column_in_file(
+                    DataCategory::PotentialPii,
+                    PII_COLUMN_NAME,
+                    TransformerType::Scramble,
+                ),
+                column_in_file(
+                    DataCategory::CommerciallySensitive,
+                    COMMERCIALLY_SENSITIVE_COLUMN_NAME,
+                    TransformerType::Scramble,
+                ),
+            ],
+        }];
+
+        let parsed = parse(
+            strategies,
+            TransformerOverrides {
+                allow_potential_pii: true,
+                allow_commercially_sensitive: false,
+            },
+        );
+
+        let pii_column_transformer = transformer_for_column(PII_COLUMN_NAME, &parsed);
+        let commercially_sensitive_transformer =
+            transformer_for_column(COMMERCIALLY_SENSITIVE_COLUMN_NAME, &parsed);
+
+        assert_eq!(pii_column_transformer.name, TransformerType::Identity);
+        assert_eq!(pii_column_transformer.args, None);
+
+        assert_eq!(
+            commercially_sensitive_transformer.name,
+            TransformerType::Scramble
+        );
+        assert_eq!(commercially_sensitive_transformer.args, None);
+    }
+
+    #[test]
+    fn ignores_transformers_for_commercially_sensitive_if_flag_provided() {
+        let strategies = vec![StrategyInFile {
+            table_name: TABLE_NAME.to_string(),
+            description: "description".to_string(),
+            columns: vec![
+                column_in_file(
+                    DataCategory::PotentialPii,
+                    PII_COLUMN_NAME,
+                    TransformerType::Scramble,
+                ),
+                column_in_file(
+                    DataCategory::CommerciallySensitive,
+                    COMMERCIALLY_SENSITIVE_COLUMN_NAME,
+                    TransformerType::Scramble,
+                ),
+            ],
+        }];
+
+        let parsed = parse(
+            strategies,
+            TransformerOverrides {
+                allow_potential_pii: false,
+                allow_commercially_sensitive: true,
+            },
+        );
+
+        let commercially_sensitive_transformer =
+            transformer_for_column(COMMERCIALLY_SENSITIVE_COLUMN_NAME, &parsed);
+        let pii_column_transformer = transformer_for_column(PII_COLUMN_NAME, &parsed);
+
+        assert_eq!(
+            commercially_sensitive_transformer.name,
+            TransformerType::Identity
+        );
+        assert_eq!(commercially_sensitive_transformer.args, None);
+
+        assert_eq!(pii_column_transformer.name, TransformerType::Scramble);
+        assert_eq!(pii_column_transformer.args, None);
+    }
+
+    #[test]
+    fn can_combine_override_flags() {
+        let strategies = vec![StrategyInFile {
+            table_name: TABLE_NAME.to_string(),
+            description: "description".to_string(),
+            columns: vec![
+                column_in_file(
+                    DataCategory::PotentialPii,
+                    PII_COLUMN_NAME,
+                    TransformerType::Scramble,
+                ),
+                column_in_file(
+                    DataCategory::CommerciallySensitive,
+                    COMMERCIALLY_SENSITIVE_COLUMN_NAME,
+                    TransformerType::Scramble,
+                ),
+            ],
+        }];
+
+        let parsed = parse(
+            strategies,
+            TransformerOverrides {
+                allow_potential_pii: true,
+                allow_commercially_sensitive: true,
+            },
+        );
+
+        let commercially_sensitive_transformer =
+            transformer_for_column(COMMERCIALLY_SENSITIVE_COLUMN_NAME, &parsed);
+        let pii_column_transformer = transformer_for_column(PII_COLUMN_NAME, &parsed);
+
+        assert_eq!(
+            commercially_sensitive_transformer.name,
+            TransformerType::Identity
+        );
+        assert_eq!(pii_column_transformer.name, TransformerType::Identity);
+    }
+
+    fn transformer_for_column(
+        column_name: &str,
+        strategies: &HashMap<String, HashMap<String, ColumnInfo>>,
+    ) -> Transformer {
+        strategies[TABLE_NAME][column_name].transformer.clone()
+    }
+
+    fn column_in_file(
+        data_category: DataCategory,
+        name: &str,
+        transformer_type: TransformerType,
+    ) -> ColumnInFile {
+        ColumnInFile {
+            data_category,
+            description: name.to_string(),
+            name: name.to_string(),
+            transformer: Transformer {
+                name: transformer_type,
+                args: None,
+            },
+        }
+    }
+}
---
+++ src/parsers/strategy_file.rs
@@ -0,0 +1,257 @@
+use crate::parsers::strategies_parser;
+use crate::parsers::strategy_structs::*;
+use itertools::sorted;
+use itertools::Itertools;
+use std::io::Write;
+
+use serde_json;
+use std::collections::HashMap;
+use std::fs;
+
+pub fn read(file_name: &str, transformer_overrides: TransformerOverrides) -> Strategies {
+    match read_file(file_name) {
+        Ok(strategies) => strategies_parser::parse(strategies, transformer_overrides),
+        Err(error) => panic!("Unable to read strategy file: {:?}", error),
+    }
+}
+
+pub fn sync_to_file(
+    file_name: &str,
+    missing_columns: Vec<SimpleColumn>,
+    redundant_columns: Vec<SimpleColumn>,
+) -> std::io::Result<()> {
+    let current_file_contents = read_file(file_name).unwrap();
+
+    let file_contents_with_missing = add_missing(current_file_contents, &missing_columns);
+    let new_file_contents = remove_redundant(file_contents_with_missing, &redundant_columns);
+
+    let file = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(file_name)?;
+
+    serde_json::to_writer_pretty(file, &new_file_contents)?;
+
+    Ok(())
+}
+
+fn add_missing(present: Vec<StrategyInFile>, missing: &[SimpleColumn]) -> Vec<StrategyInFile> {
+    let missing_columns_by_table = missing.iter().fold(HashMap::new(), |mut acc, column| {
+        acc.entry(column.table_name.clone())
+            .or_insert(Vec::new())
+            .push(column.column_name.clone());
+        acc
+    });
+
+    let mut new_strategies = present;
+
+    for (table, missing_columns) in missing_columns_by_table {
+        match new_strategies.iter().position(|c| c.table_name == table) {
+            Some(position) => {
+                let existing_table = new_strategies.get_mut(position).unwrap();
+                for column in missing_columns {
+                    existing_table.columns.push(ColumnInFile::new(&column));
+                }
+            }
+            None => {
+                let mut new_table = StrategyInFile {
+                    table_name: table.clone(),
+                    description: "".to_string(),
+                    columns: vec![],
+                };
+                for column in missing_columns {
+                    new_table.columns.push(ColumnInFile::new(&column));
+                }
+                new_strategies.push(new_table);
+            }
+        }
+    }
+
+    new_strategies.sort();
+
+    new_strategies
+}
+
+fn remove_redundant(
+    existing: Vec<StrategyInFile>,
+    redundant_columns_to_remove: &[SimpleColumn],
+) -> Vec<StrategyInFile> {
+    let table_names = redundant_columns_to_remove
+        .iter()
+        .fold(HashMap::new(), |mut acc, column| {
+            acc.entry(column.table_name.clone())
+                .or_insert(Vec::new())
+                .push(column.column_name.clone());
+            acc
+        });
+
+    existing
+        .into_iter()
+        .filter_map(
+            |strategy| match table_names.get(&strategy.table_name.clone()) {
+                Some(columns_to_remove) => {
+                    let new_columns: Vec<ColumnInFile> = strategy
+                        .columns
+                        .clone()
+                        .into_iter()
+                        .filter(|col| !columns_to_remove.contains(&col.name))
+                        .collect();
+
+                    if new_columns.is_empty() {
+                        None
+                    } else {
+                        let mut new_strategy = strategy;
+                        new_strategy.columns = new_columns;
+                        Some(new_strategy)
+                    }
+                }
+                None => Some(strategy),
+            },
+        )
+        .collect()
+}
+
+pub fn to_csv(strategy_file: &str, csv_output_file: &str) -> std::io::Result<()> {
+    let strategies = read_file(strategy_file)?;
+    let p: Vec<String> = strategies
+        .iter()
+        .flat_map(|strategy| {
+            strategy.columns.iter().filter_map(|column| {
+                if column.data_category == DataCategory::Pii
+                    || column.data_category == DataCategory::PotentialPii
+                {
+                    Some(format!(
+                        "{}, {}, {}",
+                        strategy.table_name, column.name, column.description
+                    ))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect::<Vec<String>>();
+    let to_write = format!(
+        "{}\n{}",
+        "table name, column name, description",
+        sorted(p).join("\n")
+    );
+
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(csv_output_file)?;
+    file.write_all(to_write.as_bytes()).unwrap();
+
+    Ok(())
+}
+
+fn read_file(file_name: &str) -> serde_json::Result<Vec<StrategyInFile>> {
+    match fs::read_to_string(file_name) {
+        Ok(file_contents) => {
+            let p: Vec<StrategyInFile> = serde_json::from_str(&file_contents)?;
+            Ok(p)
+        }
+
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => Ok(vec![]),
+            _ => panic!("Unable to read strategy file at {}: {:?}", file_name, e),
+        },
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn adds_missing_columns() {
+        let present = vec![StrategyInFile {
+            table_name: "public.person".to_string(),
+            description: "".to_string(),
+            columns: vec![ColumnInFile::new("id"), ColumnInFile::new("first_name")],
+        }];
+
+        let missing = vec![
+            SimpleColumn {
+                table_name: "public.person".to_string(),
+                column_name: "last_name".to_string(),
+            },
+            SimpleColumn {
+                table_name: "public.location".to_string(),
+                column_name: "id".to_string(),
+            },
+            SimpleColumn {
+                table_name: "public.location".to_string(),
+                column_name: "post_code".to_string(),
+            },
+        ];
+
+        let result = add_missing(present, &missing);
+
+        let expected = vec![
+            StrategyInFile {
+                table_name: "public.location".to_string(),
+                description: "".to_string(),
+                columns: vec![ColumnInFile::new("id"), ColumnInFile::new("post_code")],
+            },
+            StrategyInFile {
+                table_name: "public.person".to_string(),
+                description: "".to_string(),
+                columns: vec![
+                    ColumnInFile::new("id"),
+                    ColumnInFile::new("first_name"),
+                    ColumnInFile::new("last_name"),
+                ],
+            },
+        ];
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn removes_redundant_columns() {
+        let existing_columns = vec![
+            StrategyInFile {
+                table_name: "public.location".to_string(),
+                description: "".to_string(),
+                columns: vec![ColumnInFile::new("id"), ColumnInFile::new("post_code")],
+            },
+            StrategyInFile {
+                table_name: "public.person".to_string(),
+                description: "".to_string(),
+                columns: vec![
+                    ColumnInFile::new("id"),
+                    ColumnInFile::new("first_name"),
+                    ColumnInFile::new("last_name"),
+                ],
+            },
+        ];
+
+        let redundant_columns_to_remove = vec![
+            SimpleColumn {
+                table_name: "public.location".to_string(),
+                column_name: "id".to_string(),
+            },
+            SimpleColumn {
+                table_name: "public.location".to_string(),
+                column_name: "post_code".to_string(),
+            },
+            SimpleColumn {
+                table_name: "public.person".to_string(),
+                column_name: "last_name".to_string(),
+            },
+        ];
+
+        let result = remove_redundant(existing_columns, &redundant_columns_to_remove);
+
+        let expected = vec![StrategyInFile {
+            table_name: "public.person".to_string(),
+            description: "".to_string(),
+            columns: vec![ColumnInFile::new("id"), ColumnInFile::new("first_name")],
+        }];
+
+        assert_eq!(result, expected);
+    }
+}
